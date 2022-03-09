a97110047af1fc448d42270e525137ddb4fc359c
Minju Ko <anabel604@ewhain.net>
2022-03-09 16:57:20 +0900

    Create hash_report.py

---
+++ src/kakao/hash_report.py
@@ -0,0 +1,14 @@
+def solution(id_list, report, k):
+    answer = [0] * len(id_list)
+    reports = {x:0 for x in id_list}
+    #id별로 신고횟수 저장
+    
+    for i in set(report):
+        reports[i.split()[1]] += 1
+        
+    for i in set(report):
+        if reports[i.split()[1]] >= k:
+            answer[id_list.index(i.split()[0])] += 1
+    
+    
+    return answer
