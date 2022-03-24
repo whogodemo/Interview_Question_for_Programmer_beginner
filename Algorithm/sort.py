ed6e7cd21e7243aa43b8d039f593760dfde28f98
Minju Ko <anabel604@ewhain.net>
2022-03-25 00:03:02 +0900

    Update sort.py

--- Algorithm/sort.py
+++ Algorithm/sort.py
@@ -14,3 +14,32 @@ for i in range(len(a), 1, -1):
     for j in range(1, i):
         if a[j] < a[j-1]:
             a[j], a[j-1] = a[j-1], a[j]
+
+#퀵정렬
+def quick_sort(array, start, end):
+    if start >= end:
+        return
+
+    pivot = start
+    left = start+1
+    right = end
+
+    while left <= right:
+        while left <= end and array[left] <= array[pivot]:
+            left += 1
+
+        while right >= start and array[right] >= array[pivot]:
+            right -= 1
+
+        if left > right:
+            array[right], array[pivot] = array[pivot], array[right]
+
+        else:
+            array[left], array[right] = array[right], array[left]
+
+
+    quick_sort(array, start, right - 1)
+    quick_sort(array, right + 1, end)
+
+
+#quick_sort(array, 0, len(array)-1)
